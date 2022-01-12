c25736c2eff45d2e3f1d3f12867011c30178c4ab
Mindy Yuan <mindy.yuan@airbnb.com>
2022-01-12 14:24:52 -0500

    move Content into react component

---
+++ custom-templates/image-grids/flex-dest/dev/js/Content.jsx
@@ -0,0 +1,31 @@
+function Content() {
+    return (
+        <div className="content">
+            <div id="asset">
+            loading...
+            </div>
+            <div className="flex-column questions">
+                <div id="questions"/>
+                <div class="flex-grow"/>
+                <div style={{ display: 'flex' }}>
+                    <a
+                        className="waves-effect waves-light btn-large"
+                        style={{ backgroundColor: 'white', color: 'black', width: '100%', marginRight: '10px' }}
+                        onclick="skip()"
+                    >
+                        Skip
+                    </a>
+                    <a
+                        className="waves-effect waves-light btn-large"
+                        style={{ backgroundColor: '#03a9f4', width: '100%' }}
+                        onclick="submit()"
+                    >
+                        Submit
+                    </a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Content;
