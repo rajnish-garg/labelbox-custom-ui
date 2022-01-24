ae58263f7bb0027f1dcc7f85baf47b345c9d423f
Mindy Yuan <mindy.yuan@airbnb.com>
2022-01-24 15:18:44 -0500

    move out remaining component + util

--- custom-templates/image-grids/flex-dest/dev/js/App.jsx
+++ custom-templates/image-grids/flex-dest/dev/js/App.jsx
@@ -2,69 +2,9 @@ import React, { useState, useEffect, useCallback } from 'react';
 import Header from './Header';
 import Content from './Content';
 import GenericImage from './GenericImage';
+import PanelInfo from './PanelInfo';
+import { get } from './utils';
 
-function PanelInfo({
-  title, 
-  description, 
-  location, 
-  where, 
-  lat, 
-  lng,
-}) {
-  useEffect(() => {
-    document.querySelector("div.flex-column.side-panel").scrollTo(0,0);
-  }, [lat, lng])
-  
-  // https://www.google.com/maps/search/?api=1&query={lat}%2C{lng}
-  // src="https://maps.google.com/maps?q=${lat},${lng}&hl=es&z=14&amp;output=embed"
-  // href="https://maps.google.com/maps?q=${lat},${lng};z=14&amp;output=embed"
-  return (
-    <>
-    <div className='listing-info-container'>
-      <div className="listing-info">
-        <b>Title</b>: {title}
-      </div>
-    </div>
-
-    <div className='listing-info-container'>
-      <div className="listing-info">
-        <b>Description</b>: {description}
-      </div>
-    </div>
-
-    <div className='listing-info-container'>
-      <div className="listing-info">
-        <b>Location</b>: {location}
-      </div>
-    </div>
-
-    <div className='listing-info-container'>
-      <div className="listing-info">
-        <b>Where You'll Be</b>: {where}
-      </div>
-    </div>
-
-    <div className='listing-info-container'>
-      <div className="listing-info">
-        <iframe width="450" height="450" frameBorder="0" scrolling="yes" marginHeight="0" marginWidth="0"
-          src={`https://maps.google.com/maps?q=${lat},${lng}&z=14&output=embed`}
-        >
-        </iframe>
-      </div>
-    </div>
-    </>
-  );
-}
-
-// Utils
-function get(url){
-  var Httpreq = new XMLHttpRequest();
-  Httpreq.open("GET", url, false);
-  Httpreq.send(null);
-  return Httpreq.responseText;
-}
-
-// Root app
 export default function App() {
   const projectId = new URL(window.location.href).searchParams.get("project");
   const [isLoading, setIsLoading] = useState(false);
---
+++ custom-templates/image-grids/flex-dest/dev/js/utils.js
@@ -0,0 +1,6 @@
+export function get(url){
+  var Httpreq = new XMLHttpRequest();
+  Httpreq.open("GET", url, false);
+  Httpreq.send(null);
+  return Httpreq.responseText;
+}
