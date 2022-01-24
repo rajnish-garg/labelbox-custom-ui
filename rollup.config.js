d01fafcba6d4dd92b23844816238975d5457f72d
Mindy Yuan <mindy.yuan@airbnb.com>
2022-01-24 14:14:25 -0500

    add replace module for rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import babel from '@rollup/plugin-babel';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import replace from '@rollup/plugin-replace';
 
 export default {
   input: "custom-templates/image-grids/flex-dest/dev/js/App.jsx",
@@ -13,9 +14,12 @@ export default {
     nodeResolve({
       extensions: [".js"],
     }),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify( 'development' )
+    }),
     babel({
       presets: ["@babel/preset-react"],
     }),
-    commonjs(),
+    commonjs()
   ]
 };
